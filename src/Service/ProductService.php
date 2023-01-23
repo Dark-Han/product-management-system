749589127c0102ec17578833e62aed7697571811
Darkhan <31818912+Dark-Han@users.noreply.github.com>
2023-01-23 22:42:09 +0600

    feat: importing xml data
    upload xml file by chunks, then after full upload send
    xml file path to messanger, then in messanger handler
    data will be written to database

--- src/Service/ProductService.php
+++ src/Service/ProductService.php
@@ -2,17 +2,47 @@
 
 namespace App\Service;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Message\ImportXmlMessage;
+use App\Message\ProductsFromXmlParsed;
 use App\Repository\ProductRepository;
+use App\ValueObject\FileChunk;
 use App\ValueObject\ProductFilter;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\MessageBusInterface;
 
-class ProductService{
+class ProductService
+{
 
-    public function __construct(private ProductRepository $productRepository)
+    public function __construct(
+        private ProductRepository     $productRepository
+        , private FileStorage         $fileStorage
+        , private MessageBusInterface $bus
+    )
     {
     }
 
-    public function getProductsByFilterParams(ProductFilter $filter){
+    public function getProductsByFilterParams(ProductFilter $filter)
+    {
         return $this->productRepository->getProductsByFilterParams($filter);
+    }
+
+    public function importFileByChunks(string $fileName, string $pathToChuck, int $chuckSerialNumber, int $totalChucksCount)
+    {
+        $chunk = new FileChunk($fileName
+            , $pathToChuck
+            , $chuckSerialNumber
+            , $chuckSerialNumber === $totalChucksCount - 1);
+
+        $uploadFileResult = $this->fileStorage->uploadFileStreamByChunks($chunk);
+
+        if ($uploadFileResult->isFullUploaded()) {
+            $this->bus->dispatch(new ImportXmlMessage($uploadFileResult->getUploadedPath()));
+        }
+
+        return 'imported';
     }
 
 }
