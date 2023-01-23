a58db12502792367916450ad67e03ee1430cbffd
Darkhan <31818912+Dark-Han@users.noreply.github.com>
2023-01-23 22:33:58 +0600

    feat: service for file upload

--- src/Service/FileStorage.php
+++ src/Service/FileStorage.php
@@ -1,27 +1,55 @@
 <?php
+
 namespace App\Service;
 
+use App\ValueObject\FileChunk;
+use App\ValueObject\UploadedFileResult;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
-class FileStorage{
+class FileStorage
+{
 
-    public function __construct(private SluggerInterface $slugger)
+    public function __construct(private string $uploadsFolder, private SluggerInterface $slugger)
     {
     }
 
-    public function upload(UploadedFile $file,string $pathToUploadFolder){
+    public function upload(UploadedFile $file, string $folder): string
+    {
         $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = $this->slugger->slug($originalFilename);
-        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+        $pathToUploadedFolder = $this->uploadsFolder . $folder;
 
         try {
-            $file->move($pathToUploadFolder, $fileName);
+            $file->move($pathToUploadedFolder, $fileName);
         } catch (FileException $e) {
             // ...
         }
 
-        return $pathToUploadFolder."/".$fileName;
+        return $pathToUploadedFolder . "/" . $fileName;
+    }
+
+    //отрефакторить
+    public function uploadFileStreamByChunks(FileChunk $fileChunk):UploadedFileResult
+    {
+        $filePath = $this->uploadsFolder.'/'.$fileChunk->fileName();
+
+        $out = fopen("{$filePath}.part", $fileChunk->serialNumber() == 0 ? "w+" : "a+");
+        if ($out) {
+            fwrite($out, $fileChunk->content());
+            fclose($out);
+        } else {
+            die("Failed to open output stream");
+        }
+
+        if ($fileChunk->isLastChunk()) {
+            rename("{$filePath}.part", $filePath);
+            return new UploadedFileResult(true,$filePath);
+        }
+
+            return new UploadedFileResult(false,$filePath);
     }
 
 }
