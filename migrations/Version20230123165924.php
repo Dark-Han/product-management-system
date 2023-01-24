4492a150da8ceb39bfb169c048f2bcd88b05802e
Darkhan <31818912+Dark-Han@users.noreply.github.com>
2023-01-24 16:59:46 +0600

    feat: add new columns to product

---
+++ migrations/Version20230123165924.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230123165924 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product ADD description_common LONGTEXT NOT NULL, ADD description_for_ozon LONGTEXT NOT NULL, ADD description_for_wildberries LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product DROP description_common, DROP description_for_ozon, DROP description_for_wildberries');
+    }
+}
